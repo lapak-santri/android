53b85960c2cb4ca11d6098ec0e02bb11a15919a7
Nadhif Mahardika <awandi8991@gmail.com>
2022-11-24 07:42:41 +0700

    LS-5 Add Error Response

--- app/src/main/java/com/example/lapaksantri/data/repositories/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/lapaksantri/data/repositories/AuthRepositoryImpl.kt
@@ -3,12 +3,17 @@ package com.example.lapaksantri.data.repositories
 import com.example.lapaksantri.data.local.data_store.DataStoreManager
 import com.example.lapaksantri.data.remote.network.AuthApiService
 import com.example.lapaksantri.data.remote.request.LoginRequest
+import com.example.lapaksantri.data.remote.request.RegisterRequest
+import com.example.lapaksantri.data.remote.response.ErrorResponse
 import com.example.lapaksantri.domain.repositories.AuthRepository
 import com.example.lapaksantri.utils.Resource
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class AuthRepositoryImpl(
     private val authApiService: AuthApiService,
@@ -52,6 +57,32 @@ class AuthRepositoryImpl(
             }
         } catch (e: Exception) {
             emit(Resource.Error("An unexpected error occurred", false))
+        }
+    }
+
+
+    override fun register(name: String, email: String, password: String): Flow<Resource<String>> = flow {
+        emit(Resource.Loading())
+        try {
+            val response = authApiService.register(
+                RegisterRequest(
+                    name = name,
+                    email = email,
+                    password = password
+                )
+            )
+            emit(Resource.Success(response.message))
+        } catch (e: Exception) {
+            when(e) {
+                is HttpException -> {
+                    val errorMessageResponseType = object : TypeToken<ErrorResponse>() {}.type
+                    val error: ErrorResponse = Gson().fromJson(e.response()?.errorBody()?.charStream(), errorMessageResponseType)
+                    emit(Resource.Error(error.errorMessageResponse.message))
+                }
+                else -> {
+                    emit(Resource.Error("An unexpected error occurred"))
+                }
+            }
         }
     }
 }
