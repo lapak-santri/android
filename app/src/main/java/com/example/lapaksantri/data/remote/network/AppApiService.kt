d71516c93e6082a68607e1c4d8dd60a5130568ae
Nadhif Mahardika <awandi8991@gmail.com>
2022-12-06 09:51:36 +0700

    LS-17 Get Article From API

--- app/src/main/java/com/example/lapaksantri/data/remote/network/AppApiService.kt
+++ app/src/main/java/com/example/lapaksantri/data/remote/network/AppApiService.kt
@@ -1,9 +1,12 @@
 package com.example.lapaksantri.data.remote.network
 
-import com.example.lapaksantri.data.remote.response.GetSlidersResponse
+import com.example.lapaksantri.data.remote.response.article.GetArticlesResponse
+import com.example.lapaksantri.data.remote.response.slider.GetSlidersResponse
 import retrofit2.http.GET
 
 interface AppApiService {
     @GET("slider")
-    suspend fun getSliders() : GetSlidersResponse
+    suspend fun getSliders(): GetSlidersResponse
+    @GET("article")
+    suspend fun getArticles(): GetArticlesResponse
 }
--- app/src/main/java/com/example/lapaksantri/data/remote/response/slider/DataSliderResponse.kt
+++ app/src/main/java/com/example/lapaksantri/data/remote/response/slider/DataSliderResponse.kt
@@ -1,4 +1,4 @@
-package com.example.lapaksantri.data.remote.response
+package com.example.lapaksantri.data.remote.response.slider
 
 
 import com.google.gson.annotations.SerializedName
